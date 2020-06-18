919027628c9449689253107e087448bdde27579e
Ashley Hoang <ashleyhoang_147@yahoo.com>
2020-06-18 09:20:34 -0400

    finished generate  result function

--- README.md
+++ README.md
@@ -6,4 +6,7 @@ Brainstorming:
 Step 1. Create an array of variables that contain the questions
 Step 2. Create variables for 3 functions: quizzing, submitting and getting results
 Step 3. Create a function to display the questions along with its answer choice
-Step 4. Create a function to display the result of the quiz
+Step 4. Create a function to compare the selection to the correct answer
+Step 5. Create a function to display the result of the quiz
+Step 6. For each correct question add 1 to score
+Step 7. Display score and all the correct answers
--- script.js
+++ script.js
@@ -2,55 +2,101 @@
 var myQuestions = [
     {
       question: "Commonly used data types DO NOT include:",
-      answers: ["strings", "booleans", "alerts", "numbers"],
-      correctAnswers: "alerts"
+      answers: {
+          a: "strings", 
+          b: "booleans", 
+          c: "alerts", 
+          d: "numbers",
+      },
+      correctAnswers: 'c'
     },
     {
       question: "The condition in an if / else statement is enclosed within ____.",
-      answers: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      correctAnswer: "parentheses"
+      answers: {
+          a: 'quotes', 
+          b: 'curly brackets', 
+          c: 'parentheses', 
+          d: 'square brackets'
+        },
+      correctAnswer: 'c'
     },
    {
     question: "What method can be used to  execute a block of code a number of times",
-    answers: ["loops", "balls", "circles", "squares"],
-    correctAnswer: "loops"
+    answers: {
+        a: 'loops', 
+        b: 'balls', 
+        c: 'circles',
+        d: 'squares'
+    },
+    correctAnswer: 'a'
    }
   ];
-  //Creating a place to put the quiz
   var quizContainer = document.getElementById('quiz');
-  //Creating a place to put the results 
   var resultsContainer = document.getElementById('results');
-  //Creating a place to submit 
   var submitButton = document.getElementById('submit');
-  //Creating a function that combine the 3 elements together
+  
   generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
+  
   function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
-
-  function showQuestions(questions, quizContainer){
-  //Creating a place to store the output
-    var output = [];
-    var answers;
- //For each question, show the question along with all of its answer choices
-    for(var i=0; i<questions.length; i++){
-        answers = [];
-        for(letter in questions[i].answers){
-            answers.push(
-				'<label>'
-					+ '<input type="radio" name="question'+i+'" value="'+letter+'">'
-					+ letter + ': '
-					+ questions[i].answers[letter]
-				+ '</label>'
-			);
-        }
-        output.push(
-			'<div class="question">' + questions[i].question + '</div>'
-			+ '<div class="answers">' + answers.join('') + '</div>'
-		);
-    }
-    quizContainer.innerHTML = output.join('');
-}
-}
+  
+      function showQuestions(questions, quizContainer){
+          // Creating a place to store the output and the answer choices
+          var output = [];
+          var answers;
+  
+          // Show each question with its answer
+          for(var i=0; i<questions.length; i++){
+              
+              // first reset the list of answers
+              answers = [];
+  
+              // for each available answer...
+              for(letter in questions[i].answers){
+  
+                  // ...add an html radio button
+                  answers.push(
+                      '<label>'
+                          + '<input type="radio" name="question'+i+'" value="'+letter+'">'
+                          + letter + ': '
+                          + questions[i].answers[letter]
+                      + '</label>'
+                  );
+              }
+  
+              // add this question and its answers to the output
+              output.push(
+                  '<div class="question">' + questions[i].question + '</div>'
+                  + '<div class="answers">' + answers.join('') + '</div>'
+              );
+          }
+  
+          // finally combine our output list into one string of html and put it on the page
+          quizContainer.innerHTML = output.join('');
+      }
 //the questions and quizContainer values will come from the generateQuiz function
 showQuestions(questions, quizContainer);
 function showResults(questions, quizContainer, resultsContainer){
-    
+    var answerContainers = quizContainer.querySelectorAll('.answers');
+//keep track of user's answer
+    var userAnswer = '';
+    var numCorrect = 0;
+    for(var i=0; i<questions.length; i++){
+        userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
+//compare user's choice to the correct answer
+        if(userAnswer===questions[i].correctAnswer){
+//add score and mark the answer green for correct selection
+            numCorrect++;
+            answerContainers[i].style.color = 'lightgreen';
+        }
+//otherwise show red and don't add score
+        else{
+            answerContainers[i].style.color = 'red';
+		}
+    }
+    resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
+}
+//click on submit button to show results and final score
+submitButton.onclick = function(){
+	showResults(questions, quizContainer, resultsContainer);
+}
+  }
